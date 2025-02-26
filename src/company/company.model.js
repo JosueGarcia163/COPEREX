87d217637cf131c0e0b020cdd4d6afbb9513aef5
Josue Garcia <jgarcia-2023324@kinal.edu.gt>
2025-02-26 01:33:35 -0600

    add: se agrego campo de registro de actualizacion

--- src/company/company.model.js
+++ src/company/company.model.js
@@ -34,6 +34,10 @@ const companySchema = Schema({
     status: {
         type: Boolean,
         default: true
+    },
+    lastUpdated: {
+        type: Number
+
     }
 },
     {
@@ -43,33 +47,41 @@ const companySchema = Schema({
 
 //Se ejecuta si se utiliza find o findOne y el resultado de la consulta se guarda en docs
 companySchema.post(["find", "findOne"], async function (docs) {
-    //Aqui obtengo el año actual
-    const currentYear = new Date().getFullYear();
-    //const currentYear = 2026
+    
+    //const currentYear = new Date().getFullYear();
+    const currentYear = 2028
 
-    //Si docs es un array eso nos quiere decir que se hizo una consulta de tipo find.
+    
     if (Array.isArray(docs)) {
-        //declaro un bucle de los documentos y se define doc para representar documentos uno por uno.
+        
         for (const doc of docs) {
+            
             if (!doc.lastUpdated || doc.lastUpdated !== currentYear) {
-            /*Restamos con la fecha actual para ver si paso un año
-            si current year que es la fecha actual - createdAt que es la fecha en la que se agrego 
-            el documento es 1, 2, 3 etc ese numero se le sumara a yearsOfExperience y digamos que 
-            yearsOfExperience era 8 al principio ahora se le sumara los años que pasaron.
-            */
-            doc.yearsOfExperience += (currentYear - doc.createdAt.getFullYear());
-            doc.lastUpdated = currentYear;
+                const yearsSinceCreated = currentYear - doc.createdAt.getFullYear();
+                
+                doc.yearsOfExperience += yearsSinceCreated;
 
-            //Para que se actualice tambien en la base de datos.
-            await doc.save();
+                
+                doc.lastUpdated = currentYear;  
+             
+
+                // Guardamos los cambios en la base de datos
+                await doc.save();
             }
-        };
+        }
 
         //Si docs no es un array osea se utilo la funcion findOne.
     } else if (docs) {
 
-        docs.yearsOfExperience += (currentYear - docs.createdAt.getFullYear());
-        await docs.save();
+        const yearsSinceCreated = currentYear - docs.createdAt.getFullYear();
+
+        
+        if (!docs.lastUpdated || docs.lastUpdated !== currentYear) {
+            docs.yearsOfExperience = docs.yearsOfExperience + yearsSinceCreated;
+            docs.lastUpdated = currentYear;  
+
+            await docs.save();  
+        }
     }
 });
 
